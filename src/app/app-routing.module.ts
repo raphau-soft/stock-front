0ad3d743a018820ec51c55f313eacee0c77729c3
ralph <piszkorafal@gmail.com>
2020-08-21 12:16:20 +0200

    front-back login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { BoardUserComponent } from './board-user/board-user.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'signup', component: RegisterComponent },
+  { path: 'signin', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'user', component: BoardUserComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from './_services/token-storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'stock';
+  private roles: string[];
+  isLoggedIn = false;
+  username: string;
+
+  constructor(private token: TokenStorageService){}
+
+  ngOnInit(): void{
+    this.isLoggedIn = !!this.token.getToken();
+
+    if(this.isLoggedIn){
+      const user = this.token.getUser();
+      this.roles = user.roles;
+      this.username = user.username;
+    }
+  }
+
+  logout(): void{
+    this.token.signOut();
+    window.location.reload();
+  }
 }
